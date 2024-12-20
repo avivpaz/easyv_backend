4aba7c3dac1dd9fbefdcadddedf68f2c6305b206
avivpaz <avivpaz43556@gmail.com>
2024-12-20 09:08:57 +0200

    A

--- src/config/database.js
+++ src/config/database.js
@@ -16,18 +16,17 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
-    console.log('Attempting to connect to MongoDB...');
     await mongoose.connect(process.env.MONGODB_URI, {
-      authMechanism: 'SCRAM-SHA-1', // Change from default SCRAM-SHA-256
       tls: true,
-      tlsInsecure: true,
-      directConnection: true,
-      authSource: 'admin'
+      tlsCAFile: '../../certs/rds-combined-ca-bundle.pem', // Path to the certificate
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      authSource: 'admin',
     });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
-    throw error;
+    process.exit(1);
   }
 }
 
